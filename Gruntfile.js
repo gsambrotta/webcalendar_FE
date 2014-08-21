7a33fb8245bb8c1858ee08378ee45d4cbab84b2f
Giorgia Sambrotta <g.sambrotta@gmail.com>
2014-08-21 19:51:48 +0200

    fillin bower.json and gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt){
+
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-ember-templates');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					debugInfo: true,
+					noCache: true
+				},
+				files: {
+					'app/stylesheets/all.css': 'app/stylesheets/all.scss' 
+				}
+			}
+		},
+
+		emberTemplates: {
+			compile: {
+				options: {
+					amd: true,
+					preprocess: function(source){
+						return source.replace(/\s+g/, '');
+					}
+				},
+				files: {
+					"app/templates/compiledTemplates.js": "app/templates/*.handlebars"
+				}	
+			}
+		},
+
+		jshint: {
+			files: ['Gruntfile.js', 'app/**/*.js'],
+			options: {
+					globals: {
+						jQuery: true,
+						console: true,
+						module: true,
+						document: true
+					}
+			}
+		},
+
+		uglify: {
+		  options: {
+			banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+		  },
+		  dist: {
+			files: {
+			  'app/all.min.js': ['app/*.js']
+			}
+		  }
+		},
+
+		watch: {
+			options: {
+		      livereload: true,
+		    },
+			gruntfile: {
+				files: 'Gruntfile.js',
+				tasks: ['jshint:gruntfile'],
+			},
+			scripts: {
+				files: ['app/ember/**/*.js', 'app/stylesheets/**/*.scss', 'app/templates/**/*.handlebars'],
+				tasks: ['development', 'jshint', 'sass', 'emberTemplates']
+			}
+		}
+	});
+
+	grunt.registerTask('dev', ['sass', 'emberTemplates', 'jshint', 'watch']);
+	grunt.registerTask('production', ['sass', 'emberTemplates', 'jshint', 'uglify']);
+	grunt.registerTask('test', ['jshint']);
+};
