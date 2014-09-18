1b8f088975a8c811aad2f2761e1716afb87d1007
Giorgia Sambrotta <g.sambrotta@gmail.com>
2014-09-19 00:19:49 +0200

    display today's date

--- app/ember/models/cal_model.js
+++ app/ember/models/cal_model.js
@@ -15,33 +15,33 @@ WebCalendar.Cal.FIXTURES = [
 		"days_label": ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
 
 		"months_label": ['January', 'February', 'March', 'April',
-	                     'May', 'June', 'July', 'August', 'September',
-	                     'October', 'November', 'December'],
+										 'May', 'June', 'July', 'August', 'September',
+										 'October', 'November', 'December'],
 
-	  "days_per_month": [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+		"days_per_month": [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
 	}
 ];
 
 
 DS.JSONTransforms.array = {
-  serialize: function(jsonData) {
-    if (Em.typeOf(jsonData) === 'array') {
-      return jsonData;
-    } else {
-      return [];
-    }
-  },
-  deserialize: function(externalData) {
-    switch (Em.typeOf(externalData)) {
-      case 'array':
-        return externalData;
-      case 'string':
-        return externalData.split(',').map(function(item) {
-          return jQuery.trim(item);
-        });
-      default:
-        return [];
-    }
-  }
+	serialize: function(jsonData) {
+		if (Em.typeOf(jsonData) === 'array') {
+			return jsonData;
+		} else {
+			return [];
+		}
+	},
+	deserialize: function(externalData) {
+		switch (Em.typeOf(externalData)) {
+			case 'array':
+				return externalData;
+			case 'string':
+				return externalData.split(',').map(function(item) {
+					return jQuery.trim(item);
+				});
+			default:
+				return [];
+		}
+	}
 };
 
